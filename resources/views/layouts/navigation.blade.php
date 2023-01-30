c336ea2f70f2ea6294be2b0956359b2be7d64375
agucadiz <agustin.pedrote@iesdonana.org>
2023-01-30 09:27:30 +0100

    link a register

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -18,6 +18,9 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('register')" :active="request()->routeIs('dashboard')">
+                        Registrar
+                    </x-nav-link>
                 </div>
             </div>
 
